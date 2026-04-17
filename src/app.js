17183c8e55450917d132d32c65c2ebe9bfce5c47
Shruti <your-email@example.com>
2026-04-17 15:58:42 +0530

    Added home route

--- src/app.js
+++ src/app.js
@@ -31,3 +31,7 @@ app.post('/request', (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+app.get('/', (req, res) => {
+  res.send("Library DevOps App is running 🚀");
+});
